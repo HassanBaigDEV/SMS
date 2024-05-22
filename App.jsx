cfdd122da6aa605e8c42dde32b0303e383e0364f
hassanbaigdev <hassanbaig923@gmail.com>
2024-05-22 06:38:28 +0500

    admin nav cont.

--- App.jsx
+++ App.jsx
@@ -1,9 +1,5 @@
 import {View, Text} from 'react-native';
 import Navigator from './src/navigation/navigator';
 export default function App() {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+  return <Navigator />;
 }
