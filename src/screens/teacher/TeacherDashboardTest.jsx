02ba987518fc309f7266bc6d7107d9564896014a
Abdul Rafay <rafaybbx@gmail.com>
2024-05-30 04:00:59 +0500

    latest additions in teacher

--- src/screens/teacher/TeacherDashboardTest.jsx
+++ src/screens/teacher/TeacherDashboardTest.jsx
@@ -1,185 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList, StyleSheet, TouchableOpacity, ActivityIndicator,ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { FIREBASE_AUTH ,FIREBASE_DB} from '../../firebase/firebaseConfig';
-import { signOut } from 'firebase/auth';
-import {doc, getDoc,getDocs, getFirestore, collection, query, where} from 'firebase/firestore';
-import 'firebase/firestore';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { FontSize, Color, FontFamily, Border } from "../../../GlobalStyles";
-import RNPickerSelect from 'react-native-picker-select';
-import TeacherHeader from '../../components/teacherheader';
 
+import { collection, getDocs } from 'firebase/firestore';
+import { FIREBASE_DB } from '../../firebase/firebaseConfig';
 
-
-
-
-
-
-
-
-  const TeacherScreen = ({route, navigation}) => {
+const TeacherScreen = ({ route, navigation }) => {
   const { teacher } = route.params;
   const [students, setStudents] = useState([]);
-
-
-  const [academicYearDetailss, setacademicYearDetails] = useState();
-
-  const [classId, setClassid] = useState('');
-  
-  const [selectedStudent, setSelectedStudent] = useState(null);
-  const [name, setName] = useState('');
-  const [firstTerm, setFirstTerm] = useState('');
-  const [midTerm, setMidTerm] = useState('');
-  const [finalTerm, setFinalTerm] = useState('');
-  const [search, setSearch] = useState('');
-  const [isFormVisible, setIsFormVisible] = useState(false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-if(teacher){
-const getAcademicYearDetails = (teacher) => {
-  const academicYears = teacher.academicYear;
-  const years = Object.keys(academicYears); // Get all years
-  const details = years.map(year => {
-    return {
-      year: year,
-      classAssigned: academicYears[year].classAssigned,
-      subject: academicYears[year].subject
-    };
-  });
-  return details;
-};
-
-// Usage
-const academicYears = getAcademicYearDetails(teacher);
-console.log("academicYears");
-console.log(academicYears);
-
-}
-
-
+  // const [selectedYear, setSelectedYear] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const academicYearDetails = Object.keys(teacher.academicYear).map(year => ({
     label: year,
     value: year
   }));
 
-  
+  const getClassForYear = (year) => {
+    return teacher.academicYear[year]?.classAssigned || '';
+  };
 
-
-
-
-const getClassForYear = (year) => {
-  const a =teacher.academicYear[year]?.classAssigned || '';
-console.log("*******************************")
-console.log(a)
-setClassid(a);
-return a;
-
-};
-
+  const fetchStudents = async (year) => {
+    try {
+      setLoading(true);
+      const classId = getClassForYear(year);
+      const studentsCollectionRef = collection(
+        FIREBASE_DB,
+        `classes/${classId}/${year}`
+      );
+      const studentsSnapshot = await getDocs(studentsCollectionRef);
+      const studentsList = studentsSnapshot.docs.map(doc => doc.data());
+      setStudents(studentsList);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      setLoading(false);
+    }
+  };
 const [selectedYear, setSelectedYear] = useState(academicYearDetails.length > 5 ? academicYearDetails[5].value : null);
 
+  useEffect(() => {
+    if (selectedYear) {
+      fetchStudents(selectedYear);
+    }
+  }, [selectedYear]);
 
+  const renderItem = ({ item }) => (
+    <Text style={styles.studentName}>{item.studentName}</Text>
+  );
 
-
-const toclass = getClassForYear(selectedYear);
-
-const fetchStudents = async (year) => {
-  try {
-    // setLoading(true);
-    const studentsCollectionRef = collection(
-      FIREBASE_DB,
-      `classes/${classId}/${year}`
-    );
-    const studentsSnapshot = await getDocs(studentsCollectionRef);
-    const studentsList = studentsSnapshot.docs.map(doc => doc.data());
-    setStudents(studentsList);
-    // setLoading(false);
-  } catch (error) {
-    console.error('Error fetching students:', error);
-    setLoading(false); // Make sure to stop loading even if an error occurs
-  }
-};
-
-useEffect(() => {
-  if (classId && selectedYear) {
-    fetchStudents(selectedYear);
-  }
-}, [getClassForYear(selectedYear), selectedYear]);
-
-
-
-
-// const handleLogout = async () => {
-//     try {
-//       // setShowLogoutModal(true);
-//       await signOut(FIREBASE_AUTH);
-//       console.log('Logout Successful!');
-//     } catch (error) {
-//       console.error('Logout Error:', error.message);
-//     } finally {
-//       setTimeout(() => {
-//         setShowLogoutModal(false);
-//         navigation.navigate('TeacherLogin');
-//       }, 3000);
-//     }
-//   };
-
-
-
-return (
-  <ScrollView style={styles.container}>
-    <View style={styles.header}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Welcome, {teacher.teacherName}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Welcome, {teacher.teacherName}</Text>
+        </View>
       </View>
+      <Text style={styles.classText}>Class for {selectedYear}: {getClassForYear(selectedYear)}</Text>
+      <Picker
+        selectedValue={selectedYear}
+        onValueChange={(itemValue, itemIndex) => setSelectedYear(itemValue)}
+        style={styles.picker}
+      >
+        {academicYearDetails.map((yearDetail, index) => (
+          <Picker.Item key={index} label={yearDetail.label} value={yearDetail.value} />
+        ))}
+      </Picker>
+      {loading ? (
+        <ActivityIndicator size="large" color="blue" />
+      ) : (
+        <FlatList
+          data={students}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          style={styles.flatList}
+        />
+      )}
     </View>
-    <Text style={styles.classText}>Class for {selectedYear}: {getClassForYear(selectedYear)}</Text>
-    <Picker
-      selectedValue={selectedYear}
-      onValueChange={(itemValue, itemIndex) => setSelectedYear(itemValue)}
-      style={styles.picker}
-    >
-      {academicYearDetails.map((yearDetail, index) => (
-        <Picker.Item key={index} label={yearDetail.label} value={yearDetail.value} />
-      ))}
-    </Picker>
-
-    <View>
-      <Text>Hi</Text>
-    
-    </View>
-
-
-  </ScrollView>
-);
-
-
-
-
-
-
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-    // flex: 1,
-    // padding: 16,
+  container: {
+    flex: 1,
   },
   header: {
     backgroundColor: "#473f97",
@@ -188,11 +91,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 20,
-    
-  },
-  headerText: {
-    color: '#FFFFFF',
-    fontSize: 18,
   },
   titleContainer: {
     flexDirection: 'row',
@@ -203,81 +101,26 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
+    color: 'white',
   },
   classText: {
     fontSize: 18,
     marginLeft: 20,
   },
-
-
-
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-  },
-  studentItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: 'gray',
-  },
-  studentInfo: {
-    flex: 1,
-  },
-  studentName: {
-    fontWeight: 'bold',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  addButton: {
-    marginTop: 16,
-    backgroundColor: 'blue',
-    padding: 16,
-    alignItems: 'center',
-  },
-  addButtonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-   picker: {
+  picker: {
     width: '30%',
     height: 50,
     borderWidth: 1,
     borderColor: 'gray',
   },
-});
-
-
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
+  studentName: {
+    fontWeight: 'bold',
     fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 4,
-    color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
+    marginBottom: 10,
   },
-  inputAndroid: {
-    fontSize: 10,
-    paddingHorizontal: 2,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: 'purple',
-    borderRadius: 8,
-    color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
+  flatList: {
+    flex: 1,
   },
 });
-
 
 export default TeacherScreen;
--- src/screens/teacher/teacherDashboard.jsx
+++ src/screens/teacher/teacherDashboard.jsx
@@ -1,187 +1,105 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList, StyleSheet, TouchableOpacity, ActivityIndicator,ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, ActivityIndicator,Image, FlatList, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { FIREBASE_AUTH ,FIREBASE_DB} from '../../firebase/firebaseConfig';
-import { signOut } from 'firebase/auth';
-import {doc, getDoc,getDocs, getFirestore, collection, query, where} from 'firebase/firestore';
-import 'firebase/firestore';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { FontSize, Color, FontFamily, Border } from "../../../GlobalStyles";
-import RNPickerSelect from 'react-native-picker-select';
-import TeacherHeader from '../../components/teacherheader';
 
+import { collection, getDocs } from 'firebase/firestore';
+import { FIREBASE_DB } from '../../firebase/firebaseConfig';
+import { list } from 'firebase/storage';
 
-
-
-
-
-
-
-
-  const TeacherScreen = ({route, navigation}) => {
+const TeacherScreen = ({ route, navigation }) => {
   const { teacher } = route.params;
   const [students, setStudents] = useState([]);
-
-
-  const [academicYearDetailss, setacademicYearDetails] = useState();
-
-  // const [classId, setClassid] = useState('');
-  
-  const [selectedStudent, setSelectedStudent] = useState(null);
-  const [name, setName] = useState('');
-  const [firstTerm, setFirstTerm] = useState('');
-  const [midTerm, setMidTerm] = useState('');
-  const [finalTerm, setFinalTerm] = useState('');
-  const [search, setSearch] = useState('');
-  const [isFormVisible, setIsFormVisible] = useState(false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-if(teacher){
-const getAcademicYearDetails = (teacher) => {
-  const academicYears = teacher.academicYear;
-  const years = Object.keys(academicYears); // Get all years
-  const details = years.map(year => {
-    return {
-      year: year,
-      classAssigned: academicYears[year].classAssigned,
-      subject: academicYears[year].subject
-    };
-  });
-  return details;
-};
-
-// Usage
-const academicYears = getAcademicYearDetails(teacher);
-console.log("academicYears");
-console.log(academicYears);
-
-}
-
-
+  // const [selectedYear, setSelectedYear] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const academicYearDetails = Object.keys(teacher.academicYear).map(year => ({
     label: year,
     value: year
   }));
 
-  
+  const getClassForYear = (year) => {
+    return teacher.academicYear[year]?.classAssigned || '';
+  };
 
-
-
-
-const getClassForYear = (year) => {
-  const a =teacher.academicYear[year]?.classAssigned || '';
-console.log("*******************************")
-console.log(a)
-// setClassid(a);
-return a;
-
-};
-
+  const fetchStudents = async (year) => {
+    try {
+      setLoading(true);
+      const classId = getClassForYear(year);
+      const studentsCollectionRef = collection(
+        FIREBASE_DB,
+        `classes/${classId}/${year}`
+      );
+      const studentsSnapshot = await getDocs(studentsCollectionRef);
+      const studentsList = studentsSnapshot.docs.map(doc => doc.data());
+      setStudents(studentsList);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      setLoading(false);
+    }
+  };
 const [selectedYear, setSelectedYear] = useState(academicYearDetails.length > 5 ? academicYearDetails[5].value : null);
 
+  useEffect(() => {
+    if (selectedYear) {
+      fetchStudents(selectedYear);
+    }
+  }, [selectedYear]);
 
-
-
-const classId = getClassForYear(selectedYear);
-
-const fetchStudents = async (year) => {
-  try {
-    // setLoading(true);
-    const studentsCollectionRef = collection(
-      FIREBASE_DB,
-      `classes/${classId}/${year}`
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+          style={styles.listItem}
+          >
+          <View style={styles.listItemTextContainer}>
+            <Text style={styles.listItemText}>
+              {item.registrationNumber} - {item.studentName}
+            </Text>
+          </View>
+          <Image
+          source={require('../../assets/icons/eye.png')}
+          style={styles.editIcon}
+          />
+        </TouchableOpacity>
     );
-    const studentsSnapshot = await getDocs(studentsCollectionRef);
-    const studentsList = studentsSnapshot.docs.map(doc => doc.data());
-    setStudents(studentsList);
-    // setLoading(false);
-    console.log("students");
-    console.log(students);
-  } catch (error) {
-    console.error('Error fetching students:', error);
-    setLoading(false); // Make sure to stop loading even if an error occurs.j.
-  }
-};
 
-useEffect(() => {
-  if (classId && selectedYear) {
-    fetchStudents(selectedYear);
-  }
-}, [classId, selectedYear]);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Welcome, {teacher.teacherName}</Text>
+        </View>
+      </View>
 
+      <Text style={styles.classText}>Academic Year {selectedYear}: {getClassForYear(selectedYear)}</Text>
+      <Picker
+        selectedValue={selectedYear}
+        onValueChange={(itemValue, itemIndex) => setSelectedYear(itemValue)}
+        style={styles.picker}
+      >
+        {academicYearDetails.map((yearDetail, index) => (
+          <Picker.Item key={index} label={yearDetail.label} value={yearDetail.value} />
+        ))}
+      </Picker>
+      <View style={styles.list}>
+      {loading ? (
+        <ActivityIndicator size="large" color="blue" />
+      ) : (
+        <FlatList
+          data={students}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          style={styles.flatList}
+        />
 
-
-
-// const handleLogout = async () => {
-//     try {
-//       // setShowLogoutModal(true);
-//       await signOut(FIREBASE_AUTH);
-//       console.log('Logout Successful!');
-//     } catch (error) {
-//       console.error('Logout Error:', error.message);
-//     } finally {
-//       setTimeout(() => {
-//         setShowLogoutModal(false);
-//         navigation.navigate('TeacherLogin');
-//       }, 3000);
-//     }
-//   };
-
-
-
-return (
-  <ScrollView style={styles.container}>
-    <View style={styles.header}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Welcome, {teacher.teacherName}</Text>
+      )}
       </View>
     </View>
-    <Text style={styles.classText}>Class for {selectedYear}: {getClassForYear(selectedYear)}</Text>
-    <Picker
-      selectedValue={selectedYear}
-      onValueChange={(itemValue, itemIndex) => setSelectedYear(itemValue)}
-      style={styles.picker}
-    >
-      {academicYearDetails.map((yearDetail, index) => (
-        <Picker.Item key={index} label={yearDetail.label} value={yearDetail.value} />
-      ))}
-    </Picker>
-
-    <ScrollView>
-      <Text>Hi</Text>
-    
-    </ScrollView>
-
-
-  </ScrollView>
-);
-
-
-
-
-
-
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-    // flex: 1,
-    // padding: 16,
+  container: {
+    flex: 1,
   },
   header: {
     backgroundColor: "#473f97",
@@ -190,11 +108,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 20,
-    
-  },
-  headerText: {
-    color: '#FFFFFF',
-    fontSize: 18,
   },
   titleContainer: {
     flexDirection: 'row',
@@ -205,81 +118,56 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
+    color: 'white',
   },
   classText: {
     fontSize: 18,
     marginLeft: 20,
   },
-
-
-
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-  },
-  studentItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: 'gray',
-  },
-  studentInfo: {
-    flex: 1,
-  },
-  studentName: {
-    fontWeight: 'bold',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  addButton: {
-    marginTop: 16,
-    backgroundColor: 'blue',
-    padding: 16,
-    alignItems: 'center',
-  },
-  addButtonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-   picker: {
+  picker: {
     width: '30%',
     height: 50,
     borderWidth: 1,
     borderColor: 'gray',
   },
-});
-
-
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
+  studentName: {
+    fontWeight: 'bold',
     fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 4,
-    color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
+    marginBottom: 10,
   },
-  inputAndroid: {
-    fontSize: 10,
-    paddingHorizontal: 2,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: 'purple',
-    borderRadius: 8,
-    color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
+  flatList: {
+    flex: 1,
+  },
+    listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  listItemTextContainer: {
+    flex: 1,
+  },
+  listItemText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  editIcon: {
+    width: 23,
+    height: 23,
+    tintColor: 'rgb(0, 123, 255)',
+  },
+  list: {
+    flex: 1,
+    padding: 20,
   },
 });
-
 
 export default TeacherScreen;
